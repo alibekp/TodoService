a8269c63b4461732dd4f4d6b278539694cb317ea
alibekp <31817804+alibekp@users.noreply.github.com>
2022-05-31 01:28:59 +0600

    Added DTO and DAL projects

---
+++ TodoApiDTO.DAL/Entities/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApiDTO.DAL
+{
+    public class TodoItem
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+    }
+}
